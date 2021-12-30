ac4d758dede5aa21124ccbf3616894be661ccf81
naufal360 <naufal360.an@gmail.com>
2021-12-30 09:58:18 +0700

    add new sql data

--- perpuskita.sql
+++ perpuskita.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 24 Des 2021 pada 15.11
+-- Waktu pembuatan: 29 Des 2021 pada 17.05
 -- Versi server: 10.4.18-MariaDB
 -- Versi PHP: 8.0.3
 
@@ -42,10 +42,12 @@ CREATE TABLE `data_buku` (
 --
 
 INSERT INTO `data_buku` (`id`, `nama_peminjam`, `tgl_peminjam`, `nama_buku`, `pengarang`, `tgl_pengembalian`, `keterangan`) VALUES
-(2, 'Ahmad', '2021-12-24', 'Seni Berbicara', 'Larry King', '2022-12-08', 'Tepat Waktu'),
-(3, 'Dwi', '2021-12-25', 'The Answer', 'Alan & Barbara Pease', '2022-01-08', 'Tepat Waktu'),
-(4, 'Finneas', '2021-12-15', 'Good To Great', 'Jim Collins', '2022-02-27', 'Telat'),
-(5, 'Kunto', '2022-01-29', 'Hit Refresh', 'Satya Nadela', '2022-02-12', 'Tidak Kembali/Hilang');
+(2, 'Ahmad', '2021-01-05', 'Cedera Kepala', 'M. Z. Arifin', '2022-01-08', 'Tepat Waktu'),
+(3, 'Naufal', '2021-02-13', 'Kesejahteraan Sosial', 'Isbandi Rukminto Adi', '2021-02-27', 'Telat'),
+(6, 'Fadhil', '2021-04-14', 'Kolaborasi PHP 5 dan Mysql untuk pengembangan website + cd', 'Eko Priyo Utama', '2021-04-28', 'Tidak Kembali/Hilang'),
+(8, 'Dwi', '2021-05-08', 'Lembaga Keuangan Islam', 'Nurul Huda', '2021-05-10', 'Tepat Waktu'),
+(9, 'Pertiwi', '2021-06-08', 'Metodologi Penelitian Bidang Kesehatan ED. 2', 'Moch. Imron TA', '2021-06-28', 'Tidak Kembali/Hilang'),
+(10, 'Ani', '2021-07-01', 'Strategi Bahasa Assembler + CD', 'Jason P', '2021-07-09', 'Telat');
 
 -- --------------------------------------------------------
 
@@ -65,7 +67,12 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id_user`, `username`, `password`) VALUES
 (1, 'user1', '$2y$10$I92t0SDEIVf6WE1wowOyTusBU194ZFgyRk6n7GwIJpbPAFEKNUPNu'),
-(6, 'user2', '$2y$10$W0SE2OBf0GaPtJmeo9RFCu10l/yKjdbXCG0fS5/yjus0TUHOYLzpS');
+(6, 'user2', '$2y$10$W0SE2OBf0GaPtJmeo9RFCu10l/yKjdbXCG0fS5/yjus0TUHOYLzpS'),
+(7, 'user3', '$2y$10$pexnRrU1PhSdaMcF1Qy34uaQef0fnKsXXtPRtc23W6nptGZf1KaMW'),
+(8, 'user4', '$2y$10$sFixSCuExUoftUO2UyqNquu.jpAG2kGmn/yXbFDvI/SeeYJYilqA.'),
+(9, 'user5', '$2y$10$pD/O2PUdi6zRDG1Vr9GtrujZlZJEA/vP3O7hVWrfu.wGdFEWhyv1O'),
+(10, 'user6', '$2y$10$LUxGzX1gTvfyBXJIAk5Rv.VfhpvekejgzqNRokLg7Zd9wOO1FAk9K'),
+(11, 'user7', '$2y$10$29YS.tAdlgls3GgGPUu2zep7Gw5xTP4H/.xS38c4d2tnbEeYPzSgS');
 
 --
 -- Indexes for dumped tables
@@ -91,13 +98,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `data_buku`
 --
 ALTER TABLE `data_buku`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
